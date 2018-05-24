2493308464480a9370c3c61741b11f6bf98edb5b
sysadmin <sysadmin@ibpomke-imac044-4.local>
2018-05-24 10:45:18 -0400

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by sysadmin on 5/24/18.
+//  Copyright © 2018 sysadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by sysadmin on 5/24/18.
+//  Copyright © 2018 sysadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
